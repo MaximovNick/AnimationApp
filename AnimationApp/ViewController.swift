d8ee6c83f8769aed6522279b7385fc56ace746f7
Nikolai Maximov <kolkamaximov@gmail.com>
2022-06-26 17:43:19 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Nikolai Maksimov on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
